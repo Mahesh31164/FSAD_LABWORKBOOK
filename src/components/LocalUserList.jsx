adb65000efd31dfc27f363c257773932614025b4
Mahesh31164 <2400031164@kluniversity.in>
2026-04-07 08:32:34 +0530

    EXP11

---
+++ src/components/LocalUserList.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function LocalUserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/users.json")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Local Users</h2>
+      {users.map(user => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default LocalUserList;
